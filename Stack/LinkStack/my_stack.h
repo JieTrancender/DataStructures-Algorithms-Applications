89cca0abfb942f8e4f1128aa85ec8bad60374cab
_Trancender <trancender@vip.qq.com>
2015-10-13 22:40:58 +0800

    Create my_stack.h

---
+++ Stack/LinkStack/my_stack.h
@@ -0,0 +1,28 @@
+#ifndef MY_STACK_H
+#define MY_STACK_H
+
+/*
+@Author:Jie Mo
+@Email:582865471@vip.qq.com
+@Time:13th October
+@Github:www.github.com/JieTrancender
+*/
+
+//MLS stand for MyLinkStack
+namespace MLS
+{
+	//C++抽象类栈
+	template <typename anytype>
+	class MyStack
+	{
+	public:
+		virtual ~MyStack() {};
+		virtual bool empty() const = 0;
+		virtual int size() const = 0;
+		virtual anytype & top() = 0;
+		virtual void pop() = 0;
+		virtual void push(const anytype &element) = 0;
+	};
+}
+
+#endif //my_stack.h
